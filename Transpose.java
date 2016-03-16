e07785e00db23f8bb38bd97383d3cd0c0e571e94
Manu Shaurya <shauryamanu50@gmail.com>
2016-03-16 20:41:19 +0530

    Transpose of a mxn matrix

---
+++ Transpose.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+class Transpose{
+    public static void main(String[]args){
+        int m,n;
+        Scanner x=new Scanner(System.in);
+        System.out.println("Enter i and j in ixj matrix");
+        m=x.nextInt();
+        n=x.nextInt();
+        int[][]a=new int[m][n];
+        System.out.println("Enter numbers in the matrix");
+        int i,j;
+        for(i=0;i<m;i++){
+            for(j=0;j<n;j++){
+                a[i][j]=x.nextInt();
+            }
+        }
+        System.out.println("The matrix is");
+        for(i=0;i<m;i++){
+            for(j=0;j<n;j++){
+                System.out.println(a[i][j]);
+            }
+        }
+        int[][]b=new int[n][m];
+        for(i=0;i<n;i++){
+            for(j=0;j<m;j++){
+                b[i][j]=a[j][i];
+    
+            }
+        }
+        System.out.println("The transpose matrix is");
+        for(i=0;i<n;i++){
+            for(j=0;j<m;j++){
+                System.out.println(b[i][j]);
+            }}
+        
+    }
+}
