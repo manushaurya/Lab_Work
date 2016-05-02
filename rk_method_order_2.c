fbad4dc6a4be7e4cb8578ac56abab7007b18752d
Manu Shaurya <shauryamanu50@gmail.com>
2016-05-02 11:03:03 +0530

    Update rk_method_order_2.c

--- rk_method_order_2.c
+++ rk_method_order_2.c
@@ -7,7 +7,7 @@ return (pow(x,2)+pow(y,2));
 int main(){
 float x,y,x0,y0,h,k1,k2,k;
 int n,i;
-printf("Enter the value of x0,y0,h,x");
+printf("Enter the value of x0,y0,h,x\n");
 scanf("%f%f%f%f",&x0,&y0,&h,&x);
 n=(x-x0)/h;
 x=x0;
