94f017753259b0b2295b49ac3e7cc80dd7bf4bf3
Manu Shaurya <shauryamanu50@gmail.com>
2016-05-13 17:11:58 +0530

    Update regular_falsi.c

--- regular_falsi.c
+++ regular_falsi.c
@@ -1,38 +1,23 @@
-
-//15. Regular Falsi Method
-
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
 
 float f(float x)
 {
-return (expf(x) - (3 * x*x));
+	return (pow(x,3)-5*x+1);
 }
-
 int main()
 {
-int itr=0, max;
-float x1,x2,x3,x4,err;
-printf("\nEnter value of a, b, allowed error and max no of iterations\n");
-scanf("%f %f %f %d", &x1, &x2, &err, &max); 
-x3=((x1*f(x2))-(x2*f(x1)))/(f(x2)-f(x1));
-printf("\n\nIn iteration %d, Value of x3 =\t%f",itr+1,x3);
-do
-{
-if (f(x1)*f(x3)<0)
-x2=x3;
-else
-x1=x3;
-x4=x3;
-x3=((x1*f(x2))-(x2*f(x1)))/(f(x2)-f(x1));
-itr++;
-printf("\nIn iteration %d Value of x=\t%f", itr+1,x3);
-if (fabs(x4-x3)<err)
-{
-printf("\n\nAfter %d iteration, Root= %f", itr,x4);
-return 0;
-}
-}
-while (itr<max);
-return 0;
+	float a=0,b=1,x;
+	printf("a\t\tb\t\tx\n");
+	while(fabs(b-a)>0.00001)
+	{
+		x=(b-((b-a)/(f(b)-f(a)))*f(b));
+		printf("\n%f\t%f\t%f\n",a,b,x);
+		if((f(b)*f(x))<0)
+			b=x;
+		else
+			a=x;
+		
+	}
+	printf("Value of x is %f",x);
 }
