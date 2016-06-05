e81facccdf1ded3715013a41d6e909bc3172cbc7
Manu Shaurya <shauryamanu50@gmail.com>
2016-06-05 14:49:16 +0530

    IT491

--- java/PrimeEx.java
+++ java/PrimeEx.java
@@ -2,30 +2,32 @@ import java.util.Scanner;
 import java.io.*;
 
 class PrimeEx{
-static void validate(int n){
-int k=0,i;
-if(n<=0)
-throw new ArithmeticException("Not Possible");
-else
- { if(n==2)
-     System.out.println("No. is Prime");
-  else
-    { if(n%2==0)
-       System.out.println("No. is not prime ");
-      if(n==1)
-        System.out.println("No. is not prime");
-      else
-       {for(i=2;i<n/2;i++)
-          { if(n%i!=0)
-             k=0;
-            else
-             k=1;}
-      if(k==0)
-      System.out.println("No. is prime");
-      else
-      System.out.println("No. is not prime");} } } }
-public static void main(String[]args){
-int n;
-Scanner s=new Scanner(System.in);
-n=s.nextInt();
-validate(n); } }
+    static void validate(int n){
+        int k=0,i;
+        if(n<=0)
+        throw new ArithmeticException("Not Possible");
+        else
+        {
+            if(n==2)
+            System.out.println("No. is not prime");
+            else{
+                for(i=2;i<n/2;i++)
+                {
+                    if(n%i==0)
+                    k=0;
+                   
+                }
+                if(k==0)
+                System.out.println("No. is not prime");
+                else
+                System.out.println("No. is prime");
+            }
+        }}
+    public static void main(String[]args){
+        int n;
+        Scanner s=new Scanner(System.in);
+        n=s.nextInt();
+        validate(n);
+    }
+    }
+    
